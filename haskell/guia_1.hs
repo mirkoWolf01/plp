ec8a715ba38d7101c6f0002d24c7b9a8abad1719
mirkoWolf <mirkowolf46@gmail.com>
2025-04-01 07:16:11 -0300

    Update guia_1.hs

--- haskell/guia_1.hs
+++ haskell/guia_1.hs
@@ -34,7 +34,7 @@ suma :: (Num a) => [a] -> a
 suma    = foldr (+) 0 
 
 pertenece :: (Eq a) => a -> [a] -> Bool
-pertenece n = foldr (\x res -> (x==n) || res) False -- f x1 (f x2 (f x3  False))
+pertenece n = foldr (\x res -> (x==n) || res) False 
 
 concatenar :: [a] -> [a] -> [a]
 concatenar s1 s2    = foldr (:) s2 s1
@@ -65,5 +65,33 @@ sumaAlt' :: (Num a) => [a] -> a
 sumaAlt' = fst . foldr (\x (res, alt) -> (if alt then res - x else res + x , not alt)) (0, False)
 
 --------------------Ej4--------------------
+-- I.
+{- permutaciones :: [a] -> [[a]]
+permutaciones [x]   = [[x]]
+permutaciones ls    = concatMap (\x -> [[x]]) ls  -}
 
 --------------------Ej5--------------------
+elementosEnPosicionesPares :: [a] -> [a]    -- No esta hecho con recursion estructural, debido a que se modifica xs al hacer la recursion
+elementosEnPosicionesPares [] = []
+elementosEnPosicionesPares (x:xs) = if null xs then [x] else x : elementosEnPosicionesPares (tail xs)
+
+entrelazar :: [a] -> [a] -> [a]             -- Esta hecho con recursion estructural, debido a que no se modifica la lista a la que se le hace recursion
+entrelazar [] = id
+entrelazar (x:xs) = \ys -> if null ys then x : entrelazar xs [] else x : head ys : entrelazar xs (tail ys)
+
+-- TODO: FALTA HACER ENTRELAZAR
+
+--------------------Ej6--------------------
+recr :: (a -> [a] -> b -> b) -> b -> [a] -> b
+recr _ z [] = z
+recr f z (x : xs) = f x xs (recr f z xs)
+
+sacarUna :: Eq a => a -> [a] -> [a]
+sacarUna e = recr f []
+            where f = (\x xs rec -> if x == e then xs else x:rec)
+
+-- No funciona un esquema de recursion estructural para sacarUno porque es necesario que corte sin modificar los proximos elementos que coincidan. 
+
+insertarOrdenado :: Ord a => a -> [a] -> [a] -- Tiene un problema con un elemento, que lo pone atras. Sino funca bien.
+insertarOrdenado e = recr f [e]
+                    where f = (\x xs rec -> if x <= e && (if null xs then True else head xs >= e) then x:e:xs else x:rec)
